50953920a8e506d0ee1d7445a0f9c7bb9b48820c
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-18 16:55:34 +0530

    Update and rename MaxL.py to SingleRep.py

--- MaxL.py
+++
@@ -1,8 +0,0 @@
-s=input()
-mac=""
-m=0
-for i in range(0,len(s)):
-    if s.count(s[i])>m:
-        mac=s[i]
-        m=s.count(s[i])
-print(mac)
---
+++ SingleRep.py
@@ -0,0 +1,5 @@
+s=input()
+m=0
+for i in range(0,len(s)):
+    if s.count(s[i])==1:
+        print(s[i])
