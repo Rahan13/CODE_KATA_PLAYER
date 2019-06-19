7d08c7b417efdd798e483c07afd692f411601506
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-20 00:59:41 +0530

    Update Collinear.py

--- Collinear.py
+++ Collinear.py
@@ -5,5 +5,7 @@ if int(p1[0])==int(p2[0]) and int(p2[0])==int(p3[0]):
     print("yes")
 elif int(p1[1])==int(p2[1]) and int(p2[1])==int(p3[1]):
     print("yes")
+elif int(p1[1])==int(p1[0]) and int(p2[0])==int(p2[1]) and int(p3[0])==int(p3[1]):
+    print("yes")
 else:
     print("no")
