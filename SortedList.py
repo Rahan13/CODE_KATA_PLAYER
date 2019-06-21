4d232fc084f1d65856e7fd73c8eb7fb23b37063a
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-06-21 16:55:30 +0530

    Update and rename SortedList.py to Substr.py

--- SortedList.py
+++
@@ -1,7 +0,0 @@
-n=input()
-nums=input().split()
-nums1=sorted(nums)
-if nums == nums1:
-    print("yes")
-else:
-    print("no")
---
+++ Substr.py
@@ -0,0 +1,5 @@
+strgs=input().split()
+if strgs[1] in strgs[0]:
+    print("yes")
+else:
+    print("no")
