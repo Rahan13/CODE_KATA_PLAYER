7f1e40214a344cc4416f092959f6a7fa514d23fb
Rahan13 <50615681+Rahan13@users.noreply.github.com>
2019-07-11 08:53:10 +0530

    Update MinInt.py

--- MinInt.py
+++ MinInt.py
@@ -2,7 +2,11 @@ n=int(input())
 if n%2!=0:
     print("1")
 else:
+    a=True
     for i in range(2,n):
         if n%i==0 and int(n/i)%2!=0:
+            a=False
             print(i)
             break
+    if a:
+        print(n)
